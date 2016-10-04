a2633a9acfa71e8532d22ecb95b860a2b00adbf6
Matt Johnson <matthew.w.johnson@gmail.com>
2016-10-04 10:41:46 -0600

    Files from Zephyr.

---
+++ StaleComputers.ps1
@@ -0,0 +1,5 @@
+﻿import-module ActiveDirectory
+$date = [DateTime]::Today.AddDays(-90)
+Get-ADComputer -Filter  ‘PasswordLastSet -le $date’ -SearchBase "ou=computers,ou=Graduate School Of Social Work,OU=divisions,DC=du,DC=edu" -properties PasswordLastSet |
+ConvertTo-HTML –PreContent 'Possibly-inactive Computers' –Prop DistinguishedName,PasswordLastSet | 
+ Out-File "C:\Users\matthew.w.johnson.DU\Documents\Reports\inactiveSSWcomputers.html"
